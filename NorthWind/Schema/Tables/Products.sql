38906435fd94d4182fbf1e53e3f9c667678feb5d
Victor Hugo <victor.hugo@example.com>
2025-07-13 21:37:15 -0400

    Carga inicial del proyecto

---
+++ NorthWind/Schema/Tables/Products.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [nvarchar](40) NOT NULL,
+	[SupplierID] [int] NULL,
+	[CategoryID] [int] NULL,
+	[QuantityPerUnit] [nvarchar](20) NULL,
+	[UnitPrice] [money] NULL,
+	[UnitsInStock] [smallint] NULL,
+	[UnitsOnOrder] [smallint] NULL,
+	[ReorderLevel] [smallint] NULL,
+	[Discontinued] [bit] NOT NULL,
+	[rowversion] [timestamp] NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)
+)
